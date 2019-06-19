305761fe3fd8be522e4d0ce7ad29c318578da086
sagar <sagarsaini3993@gmail.com>
2019-06-19 09:57:59 -0400

    Initial Commit

---
+++ Test34-S19/GameScene.swift
@@ -0,0 +1,51 @@
+//
+//  GameScene.swift
+//  Test34-S19
+//
+//  Created by MacStudent on 2019-06-19.
+//  Copyright © 2019 rabbit. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var nextLevelButton:SKLabelNode!
+    
+    
+    override func didMove(to view: SKView) {
+        print("This is level 1")
+        self.nextLevelButton = self.childNode(withName: "nextLevelButton") as! SKLabelNode
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        if (touch == nil) {
+            return
+        }
+        
+        let location = touch!.location(in:self)
+        let node = self.atPoint(location)
+        
+        
+        
+        
+        // MARK: Switch Levels
+        if (node.name == "nextLevelButton") {
+            let scene = SKScene(fileNamed:"Level2")
+            if (scene == nil) {
+                print("Error loading level")
+                return
+            }
+            else {
+                scene!.scaleMode = .aspectFill
+                view?.presentScene(scene!)
+            }
+        }
+        
+    }
+}
